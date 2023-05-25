c29f9c14a2b9c39eaaeb4ff47365bb02c94bba9b
genaffleck <97855765+genaffleck@users.noreply.github.com>
2023-05-25 17:13:30 +0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+$(window).on("load",function() {
+  $('.flexslider').flexslider(
+    {
+      animation: "slide",
+      slideshowSpeed: 2000,
+      direction: "horizontal",
+      reverse: true,
+      pauseOnHover: true,  
+    }
+  );
+});
